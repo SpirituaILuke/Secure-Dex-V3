b747b67338242bbd65f655fc0da5bf3d698e5cd7
Spirit <57504683+SpirituaILuke@users.noreply.github.com>
2025-03-15 23:04:54 +0200

    Update Main.lua
    cloneref support

--- Main.lua
+++ Main.lua
@@ -1,3 +1,4 @@
+local a=Instance.new("Part")for b,c in pairs(getreg())do if type(c)=="table"and#c then if rawget(c,"__mode")=="kvs"then for d,e in pairs(c)do if e==a then getgenv().InstanceList=c;break end end end end end;local f={}function f.invalidate(g)if not InstanceList then return end;for b,c in pairs(InstanceList)do if c==g then InstanceList[b]=nil;return g end end end;if not cloneref then getgenv().cloneref=f.invalidate end
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SpirituaILuke/Secure-Dex-V3/main/Bypasses.lua", true))()
 getgenv().Bypassed_Dex = game:GetObjects("rbxassetid://9352453730")[1]
